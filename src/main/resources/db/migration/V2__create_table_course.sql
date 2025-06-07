6727070764763aa472493a0625225d6ca585e10a
marquesdavi <davibaterabatera@hotmail.com>
2025-06-07 20:59:29 -0300

    chore(migration): Create course table

--- src/main/resources/db/migration/V2__create_table_course.sql
+++ src/main/resources/db/migration/V2__create_table_course.sql
@@ -1,11 +1,13 @@
-CREATE TABLE Course (
+-- marquesdavi:1.0
+CREATE TABLE tb_course (
     id bigint(20) NOT NULL AUTO_INCREMENT,
-    createdAt datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
     title varchar(50) NOT NULL,
     description varchar(255) NOT NULL,
     instructor_id bigint(20) NOT NULL,
     status enum('BUILDING', 'PUBLISHED') CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT 'BUILDING',
-    publishedAt datetime DEFAULT NULL,
+    published_at datetime DEFAULT NULL,
+    is_active BOOLEAN NOT NULL DEFAULT true,
+    created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (id),
-    CONSTRAINT FK_Author FOREIGN KEY (instructor_id) REFERENCES User(id) ON DELETE CASCADE
+    CONSTRAINT fk_author FOREIGN KEY (instructor_id) REFERENCES tb_user(id) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci ROW_FORMAT=DYNAMIC;
