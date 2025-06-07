f65ccd55d7147ccf1fb6a8151ddea2cbd6aff3d5
marquesdavi <davibaterabatera@hotmail.com>
2025-06-07 20:56:00 -0300

    feat: Create task type concrete strategy abstraction

--- src/main/java/br/com/marques/byteclass/feature/task/repository/TaskRepository.java
+++ src/main/java/br/com/marques/byteclass/feature/task/repository/TaskRepository.java
@@ -1,4 +1,18 @@
 package br.com.marques.byteclass.feature.task.repository;
 
-public interface TaskRepository {
+import br.com.marques.byteclass.feature.course.entity.Course;
+import br.com.marques.byteclass.feature.task.entity.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Long> {
+
+    Optional<Task> findTaskByStatement(String statement);
+
+    List<Task> findAllByCourseIdOrderByTaskOrder(Course courseId);
 }
+
