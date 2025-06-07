4cc280a6689426d709c21e0676fd3735dcb5909f
marquesdavi <davibaterabatera@hotmail.com>
2025-06-07 20:58:06 -0300

    feat: Create task internal api and rest controller

--- src/main/java/br/com/marques/byteclass/feature/task/controller/TaskController.java
+++ src/main/java/br/com/marques/byteclass/feature/task/controller/TaskController.java
@@ -1,24 +1,35 @@
 package br.com.marques.byteclass.feature.task.controller;
 
-import org.springframework.http.ResponseEntity;
+import br.com.marques.byteclass.feature.task.api.TaskApi;
+import br.com.marques.byteclass.feature.task.api.dto.*;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.*;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/api/task")
+@Validated
+@RequiredArgsConstructor
 public class TaskController {
+    private final TaskApi taskApi;
 
-    @PostMapping("/task/new/opentext")
-    public ResponseEntity newOpenTextExercise() {
-        return ResponseEntity.ok().build();
+    @PostMapping("/new/opentext")
+    public ResponseEntity<Void> newOpenText(@Valid @RequestBody OpenTextRequest dto) {
+        taskApi.createOpenText(dto);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
-    @PostMapping("/task/new/singlechoice")
-    public ResponseEntity newSingleChoice() {
-        return ResponseEntity.ok().build();
+    @PostMapping("/new/singlechoice")
+    public ResponseEntity<Void> newSingleChoice(@Valid @RequestBody ChoiceRequest dto) {
+        taskApi.createSingleChoice(dto);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
-    @PostMapping("/task/new/multiplechoice")
-    public ResponseEntity newMultipleChoice() {
-        return ResponseEntity.ok().build();
+    @PostMapping("/new/multiplechoice")
+    public ResponseEntity<Void> newMultipleChoice(@Valid @RequestBody ChoiceRequest dto) {
+        taskApi.createMultipleChoice(dto);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
-
 }
